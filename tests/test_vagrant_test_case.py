63fc2ace47fe2c49a5827b8f20c2fbfd63700641
Nick Allen <nick.allen.cse@gmail.com>
2013-12-07 20:20:22 -0500

    Changed default behavior when vagrant_boxes is empty
    Set vagrant_boxes to auto populate to all boxes when left empty
    Added an additional test
    Made some minor fixes to formatting and documentation

--- tests/test_vagrant_test_case.py
+++ tests/test_vagrant_test_case.py
@@ -7,22 +7,35 @@ from vagrant.test import VagrantTestCase
 def get_vagrant_root(test_vagrant_root_path):
 	return os.path.dirname(os.path.realpath(__file__)) + '/vagrantfiles/' + test_vagrant_root_path
 
+SINGLE_BOX = get_vagrant_root('single_box')
+MULTI_BOX = get_vagrant_root('multi_box')
 
-class SingleBoxTests(VagrantTestCase):
+
+class BasicTests(VagrantTestCase):
 	"""Some tests covering the basic functionality of the VagrantTestCase class"""
 
+	vagrant_root = MULTI_BOX
+
+	def test_default_boxes_list(self):
+		"""Tests that all boxes in a Vagrantfile if vagrant_boxes is not defined"""
+		self.assertGreater(len(self.vagrant_boxes), 0)
+
+
+class SingleBoxTests(VagrantTestCase):
+	"""Tests for a single box setup"""
+
 	vagrant_boxes = ['default']
-	vagrant_root = get_vagrant_root('single_box')
+	vagrant_root = SINGLE_BOX
 
 	def test_box_starts(self):
-		self.assertEqual(self.vagrant.status()['default'], Vagrant.RUNNING)
+		self.assertEqual(self.vagrant.status()[self.vagrant_boxes[0]], Vagrant.RUNNING)
 
 
 class MultiBoxTests(VagrantTestCase):
 	"""Tests for a multiple box setup"""
 
 	vagrant_boxes = ['precise32', 'precise64']
-	vagrant_root = get_vagrant_root('multi_box')
+	vagrant_root = MULTI_BOX
 
 	def test_all_boxes_up(self):
 		for box_name in self.vagrant_boxes:
--- vagrant/test.py
+++ vagrant/test.py
@@ -1,3 +1,7 @@
+"""
+A TestCase class, tying together the Vagrant class and removing some of the boilerplate involved in writing tests
+that leverage vagrant boxes.
+"""
 from unittest import TestCase
 from vagrant import Vagrant
 
@@ -8,14 +12,15 @@ class VagrantTestCase(TestCase):
 	"""
 	TestCase class to control vagrant boxes during testing
 
-	vagrant_boxes: An iterable of vagrant boxes. A single box setup should have 'default' as the only box
+	vagrant_boxes: An iterable of vagrant boxes. If empty or None, all boxes will be used. Defaults to []
 	vagrant_root: The root directory that holds a Vagrantfile for configuration. Defaults to the working directory
 	restart_boxes: If True, the boxes will be restored to their initial states between each test, otherwise the boxes
-		will remain up. Defaults to True
+		will remain up. Defaults to False
 	"""
+
 	vagrant_boxes = []
 	vagrant_root = None
-	restart_boxes = True
+	restart_boxes = False
 
 	__initial_box_statuses = {}
 	__cleanup_actions = {
@@ -25,8 +30,21 @@ class VagrantTestCase(TestCase):
 	}
 
 	def __new__(cls, *args):
+		"""
+		Give the class access to a vagrant attribute that uses the vagrant_root attribute provided in the class definition
+		"""
 		cls.vagrant = Vagrant(cls.vagrant_root)
 		return super(VagrantTestCase, cls).__new__(cls, *args)
+
+	def __init__(self, *args, **kwargs):
+		"""Check that the vagrant_boxes attribute is not left empty, and is populated by all boxes if left blank"""
+		if not self.vagrant_boxes:
+			boxes = self.vagrant.status().keys()
+			if len(boxes) == 1:
+				self.vagrant_boxes = ['default']
+			else:
+				self.vagrant_boxes = boxes
+		super(VagrantTestCase, self).__init__(*args, **kwargs)
 
 	@classmethod
 	def tearDownClass(cls):
@@ -38,13 +56,14 @@ class VagrantTestCase(TestCase):
 	def restore_box_states(cls):
 		"""Restores all boxes to their original states"""
 		for box_name in cls.vagrant_boxes:
-				action = cls.__cleanup_actions.get(cls.__initial_box_statuses[box_name])
-				if action:
-					getattr(cls.vagrant, action)(vm_name=box_name)
+			action = cls.__cleanup_actions.get(cls.__initial_box_statuses[box_name])
+			if action:
+				getattr(cls.vagrant, action)(vm_name=box_name)
 
 	def setUp(self):
 		"""Starts all boxes before running tests"""
 		for box_name in self.vagrant_boxes:
+
 			self.__initial_box_statuses[box_name] = self.vagrant.status()[box_name]
 			self.vagrant.up(vm_name=box_name)
 
