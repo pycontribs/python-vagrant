f93acf272dbc8667768db775d19910d2461b7715
Justin Duplessis <drfoliberg@gmail.com>
2015-02-17 11:15:31 -0500

    Bump version from 0.5.2 to 0.5.4.

--- vagrant/__init__.py
+++ vagrant/__init__.py
@@ -21,7 +21,7 @@ import logging
 
 # python package version
 # should match r"^__version__ = '(?P<version>[^']+)'$" for setup.py
-__version__ = '0.5.2'
+__version__ = '0.5.4'
 
 
 log = logging.getLogger(__name__)
