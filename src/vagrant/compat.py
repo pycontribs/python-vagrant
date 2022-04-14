5c39b96ee584be2baed508f1eba54a0b812ef30b
Leonard Crestez <cdleonard@gmail.com>
2022-04-14 19:11:50 +0300

    typing: Add annotation for compat.decode
    Since python2 is no longer supported maybe this could be removed
    entirely instead.
    
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- src/vagrant/compat.py
+++ src/vagrant/compat.py
@@ -13,7 +13,7 @@ import sys
 PY2 = sys.version_info[0] == 2
 
 
-def decode(value):
+def decode(value: bytes) -> str:
     """Decode binary data to text if needed (for Python 3).
 
     Use with the functions that return in Python 2 value of `str` type and for Python 3 encoded bytes.
@@ -21,4 +21,4 @@ def decode(value):
     :param value: Encoded bytes for Python 3 and `str` for Python 2.
     :return: Value as a text.
     """
-    return value.decode(locale.getpreferredencoding()) if not PY2 else value
+    return value.decode(locale.getpreferredencoding()) if not PY2 else value  # type: ignore
